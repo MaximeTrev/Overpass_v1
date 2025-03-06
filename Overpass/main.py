9b2641d996e7e2be626b2b150764974c17ecdc13
MaximeTrev <maxime.trevisani@square-management.com>
2025-03-06 16:35:03 +0100

    Update main.py

--- Overpass/main.py
+++ Overpass/main.py
@@ -126,7 +126,7 @@ def load() :
     #st.title("Cat'Map v2")
     st.markdown("<h1 style='text-align: center; color: #bd8e43;'>Cat'Map v2</h1>", unsafe_allow_html=True)
     st.markdown('<div>', unsafe_allow_html=True)
-    SquareManagement = os.path.join(os.path.dirname(__file__), "PNG/SquareManagement.png")
+    #SquareManagement = os.path.join(os.path.dirname(__file__), "PNG/SquareManagement.png")
     #st.image(SquareManagement)
 
     # Appliquer du CSS pour positionner le logo en haut à droite
@@ -145,7 +145,7 @@ def load() :
         unsafe_allow_html=True
     )
     # Afficher le logo en haut à droite
-    st.markdown('<div class="logo-container"><img src=SquareManagement></div>', unsafe_allow_html=True)
+    st.markdown('<div class="logo-container"><img src="Overpass/PNG/SquareManagement.png"></div>', unsafe_allow_html=True)
 
     NomEntreprise = "Geolocation of company buildings by name"
     FichierCSV = "Geolocation of company buildings by csv file"

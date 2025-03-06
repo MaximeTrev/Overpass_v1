f46b580c779ccc693414733ea90e1051fe43cba7
MaximeTrev <maxime.trevisani@square-management.com>
2025-03-06 10:43:41 +0100

    Update mergeCountries.py

--- Overpass/mergeCountries.py
+++ Overpass/mergeCountries.py
@@ -3,8 +3,10 @@ import geopandas as gpd
 from shapely.geometry import Point
 
 
-    
-countries = gpd.read_file("country_worldwide/curiexplore-pays.shp")[["geometry", "name_fr"]]
+file_path = os.path.join(os.path.dirname(__file__), "country_worldwide/curiexplore-pays.shp")
+countries = gpd.read_file(file_path)[["geometry", "name_fr"]]
+#countries = gpd.read_file("country_worldwide/curiexplore-pays.shp")[["geometry", "name_fr"]]
+
 gdf_countries = gpd.GeoDataFrame(countries)
 
 def findCountry(df, Pays=[]):

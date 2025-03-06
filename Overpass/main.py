9df72f56243b73b746b15375d7b061b78dccbbd5
MaximeTrev <maxime.trevisani@square-management.com>
2025-03-06 16:46:39 +0100

    Update main.py

--- Overpass/main.py
+++ Overpass/main.py
@@ -124,7 +124,7 @@ def show_map(df):
     
 def load() :
     #st.title("Cat'Map v2")
-    st.image("Overpass/PNG/SquareManagement.png", width = 10px)
+    st.image("Overpass/PNG/SquareManagement.png", width = 100)
     st.markdown("<h1 style='text-align: center; color: #bd8e43;'>Cat'Map v2</h1>", unsafe_allow_html=True)
     st.markdown('<div>', unsafe_allow_html=True)
     st.image("Overpass/PNG/SquareManagement.png")

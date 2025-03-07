4efa72ae1dd57243cc5d2009c10b30e19e25fe6a
MaximeTrev <maxime.trevisani@square-management.com>
2025-03-07 12:38:21 +0100

    Update main.py

--- Overpass/main.py
+++ Overpass/main.py
@@ -187,11 +187,11 @@ def load() :
     # Création de la disposition en trois colonnes
     col1, col2, col3 = st.columns([1, 3, 1])  # Colonnes de tailles différentes
     with col1:
-        st.image(logo_gauche, width=100)  # Logo à gauche
+        st.image("Overpass/PNG/Logo_CatMapV2.png", width=100)  # Logo à gauche
     with col2:
         st.markdown("<h1 style='text-align: center; color: #bd8e43;'>Cat'Map v2</h1>", unsafe_allow_html=True)  # Titre centré
     with col3:
-        st.image(logo_droite, width=100)  # Logo à droite
+        st.image("Overpass/PNG/SquareManagement.png", width=100)  # Logo à droite
     
     NomEntreprise = "Geolocation of company buildings by name"
     FichierCSV = "Geolocation of company buildings by csv file"

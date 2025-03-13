60429f94ee4f951e435283cbdb13d80eaae1e64a
MaximeTrev <maxime.trevisani@square-management.com>
2025-03-13 12:19:04 +0100

    Update extractionDonnees.py

--- Overpass/extractionDonnees.py
+++ Overpass/extractionDonnees.py
@@ -8,8 +8,23 @@ def loadDatas(data, node = True):
         lat = data.lat
         lon = data.lon
     else:
-        lat = data.center["lat"]
-        lon = data.center["lon"]
+        if isinstance(data, overpy.Way):
+            lats, lons = [], []
+            for node in data.nodes:
+                lats.append(node.lat)
+                lons.append(node.lon)
+            lat = sum(lats) / len(lats)
+            lon = sum(lons) / len(lons)
+        
+        # Pour les 'relations', on fait de même en prenant les nœuds des membres
+        elif isinstance(data, overpy.Relation):
+            lats, lons = [], []
+            for member in data.members:
+                if isinstance(member, overpy.Node):
+                    lats.append(member.lat)
+                    lons.append(member.lon)
+            lat = sum(lats) / len(lats)
+            lon = sum(lons) / len(lons)
     amenity = data.tags.get("amenity")
     shop = data.tags.get("shop")
     return {"id": data.id, "name": data.tags.get("name", "Nom inconnu"),    

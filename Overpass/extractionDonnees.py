07d3740a7428de400a3514d9bc076fc5bd4a3a75
MaximeTrev <maxime.trevisani@square-management.com>
2025-03-13 12:17:42 +0100

    Update extractionDonnees.py

--- Overpass/extractionDonnees.py
+++ Overpass/extractionDonnees.py
@@ -4,12 +4,12 @@ import overpy
 import streamlit as st
     
 def loadDatas(data, node = True):
-    if node == True:
+    if node:
         lat = data.lat
         lon = data.lon
     else:
-        lat = data["center"]["lat"]
-        lon = data["center"]["lon"]
+        lat = data.center["lat"]
+        lon = data.center["lon"]
     amenity = data.tags.get("amenity")
     shop = data.tags.get("shop")
     return {"id": data.id, "name": data.tags.get("name", "Nom inconnu"),    
